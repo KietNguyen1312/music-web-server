29cfb97e5cc8f297253e2221d56b7d2c6deb65b9
kiet1312 <kietatwork@gmail.com>
2026-04-23 15:06:35 +0700

    Initial commit

---
+++ src/main/java/vn/com/music/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package vn.com.music.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
